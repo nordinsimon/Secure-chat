962613661e5e06d3fccc2766cacab6d5577cc92b
nordinsimon <112483351+nordinsimon@users.noreply.github.com>
2023-01-25 02:06:22 +0100

    Created three db insted of one

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,21 +6,20 @@ import { Low } from "lowdb";
 import { JSONFile } from "lowdb/node";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const file = join(__dirname, "../db.json");
+const file = join(__dirname, "../db/db_users.json");
 const adapter = new JSONFile(file);
-const db = new Low(adapter);
+const db_users = new Low(adapter);
+
+const file2 = join(__dirname, "../db/db_nextUuId.json");
+const adapter2 = new JSONFile(file2);
+const db_nextUuId = new Low(adapter2);
 
 const router = express.Router();
-await db.read();
 
-/*
-db.data[0] ----- nextuuid
-db.data[1] ----- Users
-db.data[2] ----- Chanels
-*/
+await db_users.read();
 
 router.get("/", (req, res) => {
-  res.status(200).send(db.data[1]);
+  res.status(200).send(db_users.data);
 });
 
 router.post("/", (req, res) => {
@@ -29,15 +28,16 @@ router.post("/", (req, res) => {
 });
 
 async function addUser(name, password) {
-  await db.read();
-  let nextuuid = db.data[0][0].nextuuid++;
-  db.data[1].push({
+  await db_nextUuId.read();
+  let nextuuid = db_nextUuId.data.nextuuid++;
+  console.log("nextuuid", nextuuid);
+  db_users.data.push({
     uuid: nextuuid,
     username: name,
     password: password,
   });
-  await db.write();
+  await db_nextUuId.write();
+  await db_users.write();
 }
-//addUser("Simon", "password_simon");
 
 export default router;
