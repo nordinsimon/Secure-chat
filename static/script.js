a2cbf78ed5208fdbb1536d11143d4bd281d5d3c0
nordinsimon <112483351+nordinsimon@users.noreply.github.com>
2023-01-28 01:53:16 +0100

    Testing started on Frontend parts

---
+++ static/script.js
@@ -0,0 +1,5 @@
+const authUsername = document.querySelector("#auth-username");
+const authPassword = document.querySelector("#auth-password");
+const authsigninButton = document.querySelector("#auth-signin");
+
+authsigninButton.addEventListener("click", async () => {});
