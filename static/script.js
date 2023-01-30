466bfa2d3e0dc237336cb5a8813121b7759f4304
nordinsimon <112483351+nordinsimon@users.noreply.github.com>
2023-01-30 01:17:36 +0100

    Rough design on frontend set

--- static/script.js
+++ static/script.js
@@ -1,5 +1,27 @@
-const authUsername = document.querySelector("#auth-username");
-const authPassword = document.querySelector("#auth-password");
+const inputauthUsername = document.getElementById("auth-username");
+const inputauthPassword = document.querySelector("#auth-password");
 const authsigninButton = document.querySelector("#auth-signin");
 
-authsigninButton.addEventListener("click", async () => {});
+const JWT_KEY = "login-jwt";
+let isLoggedIn = false;
+
+authsigninButton.addEventListener("click", async () => {
+  const user = {
+    username: inputauthUsername.value,
+    password: inputauthPassword.value,
+  };
+  const options = {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await fetch("/api/login/", options);
+  if (response.status === 200) {
+    const userToken = await response.json();
+
+    localStorage.setItem(JWT_KEY, userToken.token);
+    isLoggedIn = true;
+  }
+});
