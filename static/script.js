7c7aa253d4da92f9fc539716906dedf5e0afc186
nordinsimon <112483351+nordinsimon@users.noreply.github.com>
2023-01-30 05:50:07 +0100

    Added function for creating new message

--- static/script.js
+++ static/script.js
@@ -1,9 +1,17 @@
-const inputauthUsername = document.getElementById("auth-username");
+const inputauthUsername = document.querySelector("#auth-username");
 const inputauthPassword = document.querySelector("#auth-password");
 const authsigninButton = document.querySelector("#auth-signin");
 
+const chatMessageList = document.querySelector("#chat-messege-list");
+
+const inputNewMessage = document.querySelector("#input-new-message");
+const newMessageButton = document.querySelector("#new-message-button");
+
 const JWT_KEY = "login-jwt";
 let isLoggedIn = false;
+let loggedinUser = "Användarnamn";
+
+let currentchat = [];
 
 authsigninButton.addEventListener("click", async () => {
   const user = {
@@ -25,3 +33,69 @@ authsigninButton.addEventListener("click", async () => {
     isLoggedIn = true;
   }
 });
+
+newMessageButton.addEventListener("click", () => {
+  const newMessage = inputNewMessage.value;
+
+  inputNewMessage.value = "";
+  const date = "2023-01.30 03:45";
+
+  const element = createChatElement(newMessage, loggedinUser, date);
+  chatMessageList.appendChild(element);
+});
+
+function createChatElement(newMessage, user, timestamp) {
+  const message = document.createElement("div");
+  message.className = "message";
+
+  const messageboxLeft = document.createElement("div");
+  messageboxLeft.className = "messagebox-left";
+
+  const messageInfo = document.createElement("div");
+  messageInfo.className = "message-info";
+
+  const messageUser = document.createElement("p");
+  messageUser.className = "message-user";
+  messageUser.innerText = user;
+
+  const messageTimestamp = document.createElement("p");
+  messageTimestamp.className = "message-timestamp";
+  messageTimestamp.innerText = timestamp;
+
+  const messageMain = document.createElement("div");
+  messageMain.className = "message-main";
+
+  const messageText = document.createElement("p");
+  messageText.className = "message-text";
+  messageText.innerText = newMessage;
+
+  const messageboxRight = document.createElement("div");
+  messageboxRight.className = "messagebox-right";
+
+  const div = document.createElement("div");
+
+  //Creating buttons
+  const editButton = document.createElement("button");
+  editButton.innerText = "Edit";
+  editButton.className = "messagebox-right-edit";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+
+  messageInfo.appendChild(messageUser);
+  messageInfo.appendChild(messageTimestamp);
+
+  messageMain.appendChild(messageText);
+
+  messageboxLeft.appendChild(messageInfo);
+  messageboxLeft.appendChild(messageMain);
+
+  div.appendChild(editButton);
+  div.appendChild(deleteButton);
+  messageboxRight.appendChild(div);
+
+  message.appendChild(messageboxLeft);
+  message.appendChild(messageboxRight);
+
+  return message;
+}
